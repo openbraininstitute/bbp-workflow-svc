ffae0db2cae29b42693b33ac3697225ede7069c1
Eleftherios Zisis <eleftherios.zisis@openbraininstitute.org>
2025-02-06 15:38:25 +0100

    Make virtual lab project env vars strictly required

--- bbp_workflow_svc/environment.py
+++ bbp_workflow_svc/environment.py
@@ -6,6 +6,9 @@ functions to access environment variables required by the service.
 
 from bbp_workflow_svc.util import get_required_env
 
+PROJECT = get_required_env(name="PROJECT")
+VIRTUAL_LAB = get_required_env(name="VIRTUAL_LAB")
+
 
 def get_hpc_resource_provisioner_api_url() -> str:
     """Get the HPC resource provisioner API URL from the environment."""
